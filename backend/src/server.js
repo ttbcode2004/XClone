c1e14275f3c97f935b57d2dd606c8246f4e53789
Ton That Bach <ttbcode2004@gmail.com>
2026-03-07 16:52:34 +0700

    first commiat

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
